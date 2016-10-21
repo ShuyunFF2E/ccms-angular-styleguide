bbb2a7d1be75c1e0454bc04dd6a8e04d3f4d3550
Kuitos <kuitos.lau@gmail.com>
2016-10-21 15:59:53 +0800

    删除 image-min 依赖（导致安装慢的罪魁祸首）

--- webpack-dev.config.js
+++ webpack-dev.config.js
@@ -80,8 +80,7 @@ module.exports = {
 			{
 				test: /\.(jpe?g|png|gif|svg)$/i,
 				loaders: [
-					'file?hash=sha512&digest=hex&name=[hash:8].[ext]',
-					'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+					'file?hash=sha512&digest=hex&name=[hash:8].[ext]'
 				]
 			},
 			{
